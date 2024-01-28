7078df2d7f0520fc562b0308f18f42862e3ec99a
krishna thakur <115836360+krsnaimp@users.noreply.github.com>
2024-01-28 21:28:13 +0530

    Creating Repo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,4 +49,6 @@ dependencies {
     implementation("androidx.room:room-runtime:2.6.1")
     kapt("androidx.room:room-compiler:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
 }
